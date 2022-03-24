9d10ef356f37f9ad01931c5f6dc28e6523df6cee
Takuya Yogo <62864518+yogotaku@users.noreply.github.com>
2022-03-24 23:32:32 +0900

    add: 顧客管理用テーブルの作成

--- db/migrate/20220324142644_devise_create_users.rb
+++ db/migrate/20220324142644_devise_create_users.rb
@@ -6,6 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name,          null: false, default: ""
+      t.string :first_name_kana,    null: false, default: ""
+      t.string :last_name_kana,     null: false, default: ""
+      t.string :phone_number,       null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
