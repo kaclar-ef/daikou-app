660648431dad379ec56cbabb4aabc98d409586e3
Takuya Yogo <62864518+yogotaku@users.noreply.github.com>
2022-03-29 01:25:56 +0900

    add:代行依頼一覧表示機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @driving_requests = @user.driving_requests.page(params[:page]).per(10).order(reservation_datetime: 'DESC')
   end
 end
