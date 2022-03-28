1c863343f1f9a4fd13f16ff2babadaa264165573
Takuya Yogo <62864518+yogotaku@users.noreply.github.com>
2022-03-29 00:25:23 +0900

    add: モデルのバリデーションとアソシエーション

--- app/models/driving_request.rb
+++ app/models/driving_request.rb
@@ -1,2 +1,21 @@
 class DrivingRequest < ApplicationRecord
+  belongs_to :user
+  
+  validates :reservation_datetime, :departure_place, :destination, :car_model, :status, :receipt_id, presence: true
+
+  # 依頼日時が未来であることを検証
+  validate :reservation_datetime_cannot_be_in_the_past
+
+  validates :departure_place, :destination, length: { maximum: 255 }
+
+  validates :car_model, inclusion: { in: %w(普通(AT) 普通(MT) 中型 大型) }
+  validates :status, inclusion: { in: %w(代行依頼中 代行受領済 依頼完了済) }
+
+  validates :receipt_id, format: { with: /\A[a-zA-Z\d]{6}\z/ }
+
+  def reservation_datetime_cannot_be_in_the_past
+    if reservation_datetime.present? && reservation_datetime < Time.current
+      errors.add(:reservation_datetime, 'は未来の日時を設定してください')
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :driving_requests
+  
   validates :first_name, :last_name, :first_name_kana, :last_name_kana, :phone_number, presence: true
   validates :email, :password, :first_name, :last_name, :first_name_kana, :last_name_kana, length: { maximum: 255 }
 
